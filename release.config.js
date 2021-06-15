68c07a60155f54bf7c3a2012dea207d15c5456ab
Heera Singh <heera@Heera-Singh-MacBook-Pro.local>
2021-06-15 12:18:28 +0400

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	branches: "master",
-	repositoryUrl: "https://github.com/heerasinghmca/react-app-github-actions"
-	plugins: [
+  branches: "master",
+  repositoryUrl: "https://github.com/heerasinghmca/react-app-github-actions",
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "@semantic-release/npm"
   ]
-}
+};
