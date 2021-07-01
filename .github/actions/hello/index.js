c08ea3166fc047b650075dab12cf3042518fb32c
Heera Singh <heera@Heera-Singh-MacBook-Pro.local>
2021-07-01 12:26:46 +0400

    fix: custom1

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -6,13 +6,18 @@ try {
   core.debug("Debug message");
   core.warning("Warn message");
   core.error("Error message");
+  core.setSecret("SecretMesssage");
   const name = core.getInput("who-to-greet");
   console.log(`Hello ${name}`);
-
+  core.setSecret(name);
+  console.log(`Hello ${name}`);
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Starting Group");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup("Ending Group");
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
