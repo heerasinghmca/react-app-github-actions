85ba07219f0f060e7a1af466f23fcaa87a6d8a8e
Heera Singh <heera@Heera-Singh-MacBook-Pro.local>
2021-07-03 11:49:11 +0400

    fix: issue Create

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -8890,23 +8890,24 @@ module.exports = /******/ (function (modules, runtime) {
       const core = __webpack_require__(470);
       const github = __webpack_require__(469);
 
-      try {
-        const token = core.getInput("token");
-        const title = core.getInput("title");
-        const body = core.getInput("body");
-        const assignees = core.getInput("assignees");
-
-        const octokit = new github.GitHub(token);
-        const response = octokit.rest.issues.create({
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo,
-          title,
-          body,
-          assignees: assignees ? assignees.split("\n") : undefined
-        });
-        core.setOutput("issue", JSON.stringify(response.data));
-      } catch (error) {
-        core.setFailed(error.message);
+      async function run() {
+        try {
+          const token = core.getInput("token");
+          const title = core.getInput("title");
+          const body = core.getInput("body");
+          const assignees = core.getInput("assignees");
+          const octokit = new github.GitHub(token);
+          const response = await octokit.issues.create({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            title,
+            body,
+            assignees: assignees ? assignees.split("\n") : undefined
+          });
+          core.setOutput("issue", JSON.stringify(response.data));
+        } catch (error) {
+          core.setFailed(error.message);
+        }
       }
 
       /***/
