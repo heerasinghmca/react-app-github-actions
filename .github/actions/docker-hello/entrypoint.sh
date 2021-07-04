40cd662877ac410290f66915951d7bddccf32ebd
Heera Singh <heera@Heera-Singh-MacBook-Pro.local>
2021-07-04 10:05:24 +0400

    fix: docker Action

---
+++ .github/actions/docker-hello/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+echo "::group::Some expandable logs"
+echo "some stuff1"
+echo "some stuff2"
+echo "some stuff3"
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello"
